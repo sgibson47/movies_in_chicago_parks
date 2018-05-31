759463ef1198a342c58e15f20c34161f921dbaaf
sgibson47 <sgibson47@gmail.com>
2018-05-31 15:37:59 -0500

    built interface with fake info

--- lib/movies_in_chicago_parks/cli.rb
+++ lib/movies_in_chicago_parks/cli.rb
@@ -2,7 +2,33 @@ class CLI
 
   def call
     puts "Welcome! Welcome! Welcome!"
-    puts "--instructions/command options here ---"
+    list_movies
+    menu
   end
-  
+
+  def list_movies
+    puts "Movies in Chicago's Parks"
+    puts "1. Ferris Bueller's Day Off"
+    puts "2. The Birdcage"
+    puts "3. The Princess Bride"
+  end
+
+  def menu
+    puts "Enter the number of the movie for which you want details or exit to quit."
+    input = nil
+
+    while input != "exit"
+      input = gets.strip
+      case input
+      when "1"
+        puts "more info on showing of Ferris Bueller's Day Off."
+      when "2" 
+        puts "more info on showing of The Birdcage."
+      when "3"
+        puts "more info on showing of The Princess Bride."
+      when "exit"
+        break
+      end
+    end
+  end
 end
