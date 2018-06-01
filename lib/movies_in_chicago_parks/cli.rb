1acf525584a2dbf30c2a4660324bef6dd6330a96
sgibson47 <sgibson47@gmail.com>
2018-06-01 15:40:33 -0500

    added layer of depth to list_by_park & list_by_date

--- lib/movies_in_chicago_parks/cli.rb
+++ lib/movies_in_chicago_parks/cli.rb
@@ -11,8 +11,6 @@ class CLI
     puts "Enter -by date- to see a list of the dates on which there's a movie playing in one of Chicago's parks."
     puts "Enter -by park- to see a list of the parks in which movies are playing."
     puts "\n"
-    puts "Once you pick, you'll get to select from the list to get further details."
-    puts "\n"
     
     input = nil
 
@@ -27,7 +25,6 @@ class CLI
       when "exit"
         break
       else
-        puts "Hrm, I'm not sure what you want."
         puts "Please type by name, by date, by park, or exit."
       end
     end
@@ -35,11 +32,12 @@ class CLI
 
   def list_by_name
     puts "\n"
+    puts "Movies"
     Showing.all.each_with_index {|showing, i|
       puts "#{i + 1}. #{showing.name}"}
     puts "\n"
-    puts "Enter a number to see when and where that movie is playing,"
-    puts " or enter back to return to the main menu."
+    puts "Enter a number to see details for that movie,"
+    puts " or enter back to go back to the main menu."
 
     input = nil 
   
@@ -65,13 +63,12 @@ class CLI
 
   def list_by_date
     puts "\n"
-
+    puts "Dates"
     Gate.all.each_with_index {|date, i|
       puts "#{i + 1}. #{date.month} #{date.day}, #{date.year}"}
-    
     puts "\n"
     puts "Enter a number to see which movies are playing on that date,"
-    puts " or enter back to return to the main menu."
+    puts " or enter back to go back to the main menu."
     
     input = nil
 
@@ -81,7 +78,31 @@ class CLI
         date = Gate.all[input.to_i - 1]
         date.list_showings
         puts "\n"
-        puts "Enter another number from the list or back."
+        puts "Enter a number to see details for that showing,"
+        puts " or enter back to go back to the main menu."
+        input_ = nil
+        while input != "return"
+          input = gets.strip
+          if input.to_i > 0 && input.to_i <= date.showings.length
+            showing = date.showings[input.to_i - 1]
+            showing.display_details
+            puts "\n"
+            puts "Enter another number from the list of showings"
+            puts "or return to return to the list of dates."
+          elsif input == "return"
+            break
+          else
+            puts "I can't tell what you want."
+            puts "Please type a number from the list of showings on this day"
+            puts "or return to return to the list of dates."
+          end
+        end
+        puts "Dates"
+        Gate.all.each_with_index {|date, i|
+          puts "#{i + 1}. #{date.month} #{date.day}, #{date.year}"}
+        puts "\n"
+        puts "Enter a number to see which movies are playing on that date,"
+        puts " or enter back to go back to the main menu."
       elsif input == "back"
         break
       else
@@ -94,13 +115,13 @@ class CLI
     puts "Welcome back to the main menu."
     puts "Please type by name, by date, or by park to see a list, or exit."
   end
+ 
 
   def list_by_park
     puts "\n"
-    
+    puts "Parks"
     Park.all.each_with_index {|park, i|
       puts "#{i + 1}. #{park.name}"}
-
     puts "\n"
     puts "Enter a number to see which movies are playing in that park,"
     puts " or enter back to return to the main menu."
@@ -113,7 +134,31 @@ class CLI
         park = Park.all[input.to_i - 1]
         park.list_showings
         puts "\n"
-        puts "Enter another number from the list of parks or back."
+        puts "Enter a number to see details for that showing,"
+        puts " or enter back to return to the main menu."
+        input = nil
+        while input != "return"
+          input = gets.strip
+          if input.to_i > 0 && input.to_i <= park.showings.length
+            showing = park.showings[input.to_i - 1]
+            showing.display_details
+            puts "\n"
+            puts "Enter another number from the list of showings"
+            puts "or return to return to the list of parks."
+          elsif input == "return"
+            break
+          else
+            puts "I can't tell what you want."
+            puts "Please type a number from the list of showings in this park"
+            puts "or return to return to the list of parks."
+          end
+        end
+        puts "Parks"
+        Park.all.each_with_index {|park, i|
+          puts "#{i + 1}. #{park.name}"}
+        puts "\n"
+        puts "Enter a number to see which movies are playing in that park,"
+        puts " or enter back to go back to the main menu."
       elsif input == "back"
         break
       else
